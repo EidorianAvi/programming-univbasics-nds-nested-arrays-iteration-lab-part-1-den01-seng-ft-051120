00d687e0733cfd677e8b16d964828eb956d8f4a3
Rene Avila <reneavila1993@gmail.com>
2020-05-07 19:53:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -18,7 +18,7 @@ def find_even_values(src)
     element_index = 0 
     while element_index < src[row_index].length do
       if src[row_index][element_index] % 2 == 0 
-        return element_index
+        return src[row_index][element_index]
       end
       element_index += 1
     end
